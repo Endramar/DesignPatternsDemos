314f555cc8b909d0c84ec24b155334c94e87e460
Endramar <alikskyl@gmail.com>
2020-03-15 13:54:26 +0300

    Open closed princible demo has added

---
+++ OpenClosedDemo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenClosedDemo
+{
+    public enum Color
+    {
+        Red, Blue, Green
+    }
+
+    public enum Size
+    {
+        Small, Medium, Large
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+
+        public Product(string name, Color color, Size size)
+        {
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+            {
+                if (p.Size == size)
+                {
+                    yield return p;
+                }
+            }
+        }
+
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+            {
+                if (p.Color == color)
+                {
+                    yield return p;
+                }
+            }
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = { apple, tree, house };
+
+            var pf = new ProductFilter();
+            Console.WriteLine("Green Products (OLD) : ");
+
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                Console.WriteLine($"-- {p.Name} is a green product");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
