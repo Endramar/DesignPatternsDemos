96a0a916a2c1c4833d5f24bbbe4ab0d856d96800
Endramar <alikskyl@gmail.com>
2020-03-17 14:42:33 +0300

    fix

--- BuilderDemo/Program.cs
+++ BuilderDemo/Program.cs
@@ -42,7 +42,7 @@ namespace BuilderDemo
 
             foreach (var htmlElement in Elements)
             {
-               sb.Append(htmlElement.ToStringImpl(indent + 1));
+                sb.Append(htmlElement.ToStringImpl(indent + 1));
             }
 
             sb.AppendLine($"{indentString}</{Name}>");
@@ -68,10 +68,13 @@ namespace BuilderDemo
             this.rootElement.Name = rootName;
         }
 
-        public void AddChild(string name, string text)
+        public HtmlBuilder AddChild(string name, string text)
         {
             var childElement = new HtmlElement(text, name);
             rootElement.Elements.Add(childElement);
+
+            /// Return the reference of the builder itself so that we can call AddChild method as many times as needed without usage of the instance
+            return this;
         }
 
         public override string ToString()
@@ -98,10 +101,20 @@ namespace BuilderDemo
             // below code is the same one with custom HtmlBuilder
 
             var htmlBuilder = new HtmlBuilder("ul");
-            foreach(var word in listWords)
-            {
-                htmlBuilder.AddChild("li", word);
-            }
+
+            htmlBuilder.AddChild("li", "first test word");
+            htmlBuilder.AddChild("li", "second test word");
+
+            Console.WriteLine(htmlBuilder);
+
+            // in order to make the builder use fluent interface we need to return its reference from AddChild method
+            // By this way we can call the AddChild method like below
+
+
+            htmlBuilder = new HtmlBuilder("ul")
+           .AddChild("li", "first test word")
+           .AddChild("li", "second test word")
+           .AddChild("li", "third test word");
 
             Console.WriteLine(htmlBuilder);
 
