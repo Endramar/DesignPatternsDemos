f0245c8638007d022dd62385079eb4206dd4ef0d
Endramar <alikskyl@gmail.com>
2020-03-19 21:17:41 +0300

    singleton implementation

---
+++ SingletonImplementationDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using MoreLinq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SingletonImplementationDemo
+{
+    public interface IDatabase
+    {
+        int GetPopulationOfCity(string name);
+    }
+
+    public class SingletonDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
+
+        // make the constructor private so that noone can creat an instance
+        private SingletonDatabase()
+        {
+            WriteLine("Initializing the database...");
+            capitals = File.ReadAllLines("capitals.txt").Batch(2).ToDictionary(x => x.ElementAt(0).Trim(), x => int.Parse(x.ElementAt(1).Trim()));
+        }
+
+        public int GetPopulationOfCity(string name)
+        {
+            return capitals[name];
+        }
+
+        private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase()); // this only works once;
+
+        public static SingletonDatabase Instance => instance.Value;  // this is the singleton property
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The prgram is running.");
+
+            var db = SingletonDatabase.Instance;
+
+            Console.ReadLine();
+        }
+    }
+}
