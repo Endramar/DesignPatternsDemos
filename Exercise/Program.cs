4c420536cc8128353f0ca8eea10edb038f3f76af
Ali Kisikyol <ali@torpedotv.com>
2021-08-20 23:35:34 +0100

    per thread singleton by threadlocal class and exercise

---
+++ Exercise/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Exercise
+{
+    public sealed class SingletonClass
+    {
+        public static Lazy<SingletonClass> lazyInstance = new Lazy<SingletonClass>(() => new SingletonClass());
+        public static SingletonClass Instance => lazyInstance.Value;
+
+        public string Identity;
+
+        private SingletonClass()
+        {
+            Identity = Guid.NewGuid().ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"The identity is {Identity}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SingletonTester.IsSingleton(() => { return SingletonClass.Instance; }));
+        }
+    }
+
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+
+            var instance1 = func.Invoke();
+            var instance2 = func.Invoke();
+
+            Console.WriteLine(instance1.ToString());
+            Console.WriteLine(instance2.ToString());
+
+            return instance1.Equals(instance2);
+
+        }
+    }
+}
---
+++ PerThreadSingleton/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PerThreadSingleton
+{
+    /// <summary>
+    /// The singleton class per thread
+    /// </summary>
+    public sealed class PerThreadSingleton
+    {
+        private static ThreadLocal<PerThreadSingleton> threadInstance = new ThreadLocal<PerThreadSingleton>(() => new PerThreadSingleton());
+
+        public int Id;
+
+        //Make the constructor private to prevent instance creation
+        private PerThreadSingleton()
+        {
+            // To see the id of the current thread
+            Id = Thread.CurrentThread.ManagedThreadId;
+        }
+
+        public static PerThreadSingleton Instance => threadInstance.Value;
+    }
+
+    /// <summary>
+    /// A project for creating singleton design not for entire application but for each thread
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // First thread
+            var thread1 = Task.Run(() =>
+            {
+                Console.WriteLine("T1 : " + PerThreadSingleton.Instance.Id);
+            });
+
+            // Second thread
+            var thread2 = Task.Run(() =>
+            {
+                Console.WriteLine("T2 : " + PerThreadSingleton.Instance.Id);
+                Console.WriteLine("T2 : " + PerThreadSingleton.Instance.Id);
+            });
+
+            Task.WaitAll(thread1, thread2);
+
+        }
+    }
+}
